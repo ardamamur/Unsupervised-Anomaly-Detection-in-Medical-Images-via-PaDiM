5618f4b8de0c3b5f9d80a3c8fe05bc187e2ca337
tumamur <arda.mamur@tum.de>
2023-11-22 18:38:26 +0100

    v1.0

--- src/data/brain_mri_lightning.py
+++ src/data/brain_mri_lightning.py
@@ -1,35 +1,23 @@
-from typing import Sequence, Optional
+from typing import Sequence, Tuple, Optional
 import pytorch_lightning as pl
 import json
-from src.utils._preprare_data import _get_train_files, _get_test_files, _get_val_files
-from src.data.brain_mri_dataset import BrainMRI
+from utils._preprare_data import _get_train_files, _get_test_files, _get_val_files
+from data.brain_mri_dataset import BrainMRI_Train, BrainMRI_Eval
 from torch.utils.data import DataLoader
 
-class BrainMriLightning(pl.LightningDataModule):
-    def __init__(self, batch_size: int, crop_size: Sequence[int], split_dir:str, pathology: Optional[str] ) -> None:
+class BrainMri_Train_Lightning(pl.LightningDataModule):
+    def __init__(self, batch_size: int, crop_size: Sequence[int], split_dir:str ) -> None:
         super().__init__()
         self.batch_size = batch_size
         self.crop_size = crop_size
         self.split_dir = split_dir
-        self.pathology = pathology
 
     def setup(self, stage: str = None) -> None:
-        # This function is called on every GPU when you do model.fit()
-        # It is used to load the data from the disk
-        # This function is called on every GPU when you do model.fit()
-        # It is used to load the data from the disk
-
         if stage == 'fit' or stage is None:
             train_data = _get_train_files(self.split_dir)
             val_data = _get_val_files(self.split_dir)
-            self.train_dataset = BrainMRI(data = train_data, crop_size = self.crop_size, mode = 'train')
-            self.val_dataset = BrainMRI(data = val_data, crop_size = self.crop_size, mode = 'val')
-
-        if stage == 'test' or stage is None:
-            test_data = _get_test_files(self.split_dir, self.pathology)
-            print(test_data)
-            #assert len(test_data['img']) == len(test_data['pos_mask']) == len(test_data['neg_mask']), 'Length of the data is not equal'
-            self.test_dataset = BrainMRI(data = test_data, crop_size = self.crop_size, mode = 'test')
+            self.train_dataset = BrainMRI_Train(data = train_data, crop_size = self.crop_size, mode = 'train')
+            self.val_dataset = BrainMRI_Train(data = val_data, crop_size = self.crop_size, mode = 'val')
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=self.batch_size,
@@ -39,9 +27,47 @@ class BrainMriLightning(pl.LightningDataModule):
         # This function returns the dataloader for validation
         return DataLoader(self.val_dataset, batch_size=self.batch_size,
                                 shuffle=False, num_workers=16, pin_memory=True)
+
     
+class BrainMri_Eval_Lightning(pl.LightningDataModule):
+    def __init__(self, batch_size: int, crop_size: Sequence[int], split_dir:str) -> None:
+        super().__init__()
+        self.batch_size = batch_size
+        self.crop_size = crop_size
+        self.split_dir = split_dir
+        self.pathologies = [
+            'absent_septum',
+            'artefacts',
+            'craniatomy',
+            'dural',
+            'ea_mass',
+            'edema',
+            'encephalomalacia',
+            'enlarged_ventricles',
+            'intraventricular',
+            'lesions',
+            'mass',
+            'posttreatment',
+            'resection',
+            'sinus',
+            'wml',
+            'other'
+        ]
+
+    def setup(self, stage: str = None) -> None:
+        if stage == 'test' or stage is None:
+            test_dataset = {}
+            for pathology in self.pathologies:
+                test_data = _get_test_files(self.split_dir, pathology)
+                test_dataset[pathology] = BrainMRI_Eval(data = test_data, crop_size = self.crop_size)
+            self.test_dataset = test_dataset
+
+
     def test_dataloader(self) -> DataLoader:
-        # This function returns the dataloader for testing
-        return DataLoader(self.test_dataset, batch_size=self.batch_size,
-                                shuffle=False, num_workers=16, pin_memory=True)
-    
+        # This function returns the dataloader for validation
+        loader_dict = {}
+        for pathology in self.test_dataset.keys():
+            loader_dict[pathology] = DataLoader(self.test_dataset[pathology], batch_size=self.batch_size,
+                                    shuffle=False, num_workers=16, pin_memory=True)
+            
+        return loader_dict
--- src/utils/environment_settings.py
+++ src/utils/environment_settings.py
@@ -1,6 +1,10 @@
 from easydict import EasyDict as edict
 
 env_settings = edict()
-
-
 env_settings.DATA = '/home/mamur/TUM/MLMI/data'
+
+# defaults from the paper
+env_settings._N_FEATURES_DEFAULTS = {
+    "resnet18": 100,
+    "wide_resnet50_2": 550,
+}
