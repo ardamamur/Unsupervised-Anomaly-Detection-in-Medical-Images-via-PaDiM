31aefc8fe9b31c3f1f54fe8afa47e72e3bae2dee
tumamur <arda.mamur@tum.de>
2024-01-29 03:43:34 +0100

    added

--- fast_ixi.py
+++ fast_ixi.py
@@ -7,12 +7,15 @@ from torch.utils.data import Dataset
 from torchvision import transforms as T
 
 class FAST_IXI(Dataset):
-    def __init__(self, opt, is_train=True):
+    def __init__(self, opt, records, is_train=True, eval_class=None):
         self.opt = opt
+        self.dataset_name = opt['dataset']['name']
         self.dataset_path = opt['dataset']['path']
         self.is_train = is_train
         self.resize = self.opt['dataset']['resize']
         self.cropsize = self.opt['dataset']['cropsize']
+        self.record_df = records
+        self.eval_class = eval_class
 
         # set transforms
         self.transform_x = T.Compose([T.Resize(self.resize, Image.ANTIALIAS),
@@ -25,8 +28,12 @@ class FAST_IXI(Dataset):
                                          T.ToTensor()])
         
         # load dataset
+        if self.is_train:
+            self.x, self.y = self.load_dataset()
 
-        self.x, self.y, self.mask = self.load_dataset()
+        else:
+            self.x, self.y, self.full_maps, self.pos_masks, self.neg_masks = self.load_dataset()
+
 
     def __len__(self):
         return len(self.x)
@@ -34,56 +41,75 @@ class FAST_IXI(Dataset):
     def __getitem__(self, index):
         x = self.x[index]
         y = self.y[index]
-        mask = self.mask[index]
-
+        
         x = Image.open(x).convert('RGB')
         x = self.transform_x(x)
 
         # since only normal images will be used in training, mask image will be all zeros
-        if y == 0:
+        if self.is_train:
             mask = torch.zeros(1, self.cropsize, self.cropsize)
+            return x, y, mask
+        
+        # for abnormal images, load the mask image
         else:
-            if mask is not None:
-                mask = Image.open(mask)
-                mask = self.transform_mask(mask)
-            else:
-                mask = torch.zeros(1, self.cropsize, self.cropsize)
+            full_map = self.full_maps[index]
+            pos_mask = self.pos_masks[index]
+            neg_mask = self.neg_masks[index]
 
-        return x, y, mask
+            if full_map is not None:
+                full_map = Image.open(full_map)
+                full_map = self.transform_mask(full_map)
+            else:
+                full_map = torch.zeros(1, self.cropsize, self.cropsize)
+
+            if pos_mask is not None:
+                #print('full_map is not None')
+                pos_mask = Image.open(pos_mask)
+                pos_mask = self.transform_mask(pos_mask)
+            else:
+                pos_mask = torch.zeros(1, self.cropsize, self.cropsize)
+
+            if neg_mask is not None:
+                neg_mask = Image.open(neg_mask)
+                neg_mask = self.transform_mask(neg_mask)
+            else:
+                neg_mask = torch.zeros(1, self.cropsize, self.cropsize)
+
+            return x, y, full_map, pos_mask, neg_mask
+        
 
     def load_dataset(self):
-        phase = 'train' if self.is_train else 'val'
-        x, y, mask = [], [], []
-        
-        img_dir = os.path.join(self.dataset_path, phase)
-        img_types = os.listdir(img_dir)
-        mask_dir = os.path.join(self.dataset_path, 'mask')
+        phase = 'train' if self.is_train else 'test'
+        # get the rows where split is train or test
+        records = self.record_df[self.record_df['split'] == phase]        
+        # add new column 'label' to dataframe if type contains 'normal' add 0 else add 1
+        records['label'] = records['type'].apply(lambda x: 0 if 'normal' in x else 1)
 
-        # train -> normal 
-        # val -> normal, abnormal
-
-        for img_type in img_types:
-            # load images
-            img_type_dir = os.path.join(img_dir, img_type)
-            img_fpath_list = sorted([os.path.join(img_type_dir, f)
-                                                for f in os.listdir(img_type_dir)
-                                                if f.endswith('.png')])
-            
-            x.extend(img_fpath_list)
-
-            # load labels
-            if img_type == 'normal':
-                y.extend([0] * len(img_fpath_list))
-                mask.extend([None] * len(img_fpath_list))
+        if self.is_train:
+            if self.dataset_name == 'fast':
+                records = records[records['type'] == 'normal_fast']
+            elif self.dataset_name == 'ixi':
+                records = records[records['type'] == 'normal_ixi']
             else:
-                y.extend([1] * len(img_fpath_list))
-                mask_type_dir = os.path.join(mask_dir)
-                mask_fpath_list = sorted([os.path.join(mask_type_dir, f) 
-                                                for f in os.listdir(img_type_dir)
-                                                if f.endswith('.png')])
-                mask.extend(mask_fpath_list)
+                pass
 
+            images = records['filename'].tolist()
+            labels = records['label'].tolist()
+            assert len(images) == len(labels) # sanity check
+            return images, labels
+        
 
-        assert len(x) == len(y) # sanity check
+        else:
+            if self.eval_class is not None:
+                records = records[records['type'] == self.eval_class]
+            else:
+                pass
 
-        return list(x), list(y), list(mask)
+            images = records['filename'].tolist()
+            labels = records['label'].tolist()
+            full_maps = records['full_map'].tolist()
+            pos_masks = records['mask_pos'].tolist()
+            neg_masks = records['mask_neg'].tolist()
+            assert len(images) == len(labels) == len(pos_masks) == len(neg_masks) == len(full_maps) # sanity check
+            return images, labels, full_maps, pos_masks, neg_masks
+        
---
+++ preprocess_data.py
@@ -0,0 +1,193 @@
+import os
+from PIL import Image
+import pandas as pd
+from tqdm import tqdm
+
+
+class DataHandler():
+    def __init__(self, opt):
+        self.opt = opt
+        #self.create_records()
+
+    def create_records(self):
+        split_path = self.opt['dataset']['ann_path']
+        # get all the file names in the split path
+        split_files = os.listdir(split_path)
+        print(split_files)
+        class_names = {
+            'absent_septum' : 0,
+            'artefacts' : 0,
+            'craniatomy' : 0,
+            'dural' : 0,
+            'ea_mass' : 0,
+            'edema' : 0,
+            'encephalomalacia' : 0,
+            'enlarged_ventricles' : 0,
+            'intraventricular' : 0,
+            'lesions' : 0,
+            'local' : 0,
+            'mass' : 0,
+            'mass_all' : 0,
+            'other' : 0,
+            'posttreatment' : 0,
+            'resection' : 0,
+            'sinus' : 0,
+            'wml' : 0
+        }
+
+        # create a pandas dataframe with the column filename, type, mask_pos, mask_neg, split
+        data_df = pd.DataFrame(columns=['filename', 'type', 'full_map', 'mask_pos', 'mask_neg', 'split'])
+
+
+        for file in split_files:
+            file_path = os.path.join(split_path, file)
+            df = pd.read_csv(file_path)
+            print
+            if 'ixi' in file:
+                path = self.opt['dataset']['path']
+                df['filename'] = df['filename'].str.replace('./data', path, regex=False)
+                filenames = df['filename'].tolist()
+                num_filenames = len(filenames)
+                # create a type list with the length of filenames and append normal
+                type_list = ['normal_ixi'] * num_filenames
+                full_map_list = [None] * num_filenames
+                mask_pos_list = [None] * num_filenames
+                mask_neg_list = [None] * num_filenames
+                split_list = ['train'] * num_filenames
+
+                # apextend the dataframe with the new data
+                data_to_append = pd.DataFrame({
+                    'filename': filenames,
+                    'type': type_list,
+                    'full_map': full_map_list,
+                    'mask_pos': mask_pos_list,
+                    'mask_neg': mask_neg_list,
+                    'split': split_list
+                })
+                # Use concat instead of append
+                data_df = pd.concat([data_df, data_to_append], ignore_index=True)
+
+
+
+            else:
+
+                path = self.opt['dataset']['path']
+                if 'normal' in file:
+                    if file == 'normal_test_ann.csv':
+                        continue
+
+                    else:
+                        print(file)
+                        df['filename'] = df['filename'].str.replace('./data', path, regex=False)
+                        #df['full_map'] = df['filename']
+                        #df['full_map'] = df['full_map'].str.replace('.png', '_brain_map_full.png', regex=False)
+
+                        filenames = df['filename'].tolist()
+                        type_list = ['normal_fast'] * len(filenames)
+                        full_map_list = [None] * len(filenames)
+
+                        num_filenames = len(filenames)
+                        if file == 'normal_train.csv':
+                            split_list = ['train'] * num_filenames
+                        elif file == 'normal_val.csv':
+                            split_list = ['val'] * num_filenames
+                        elif file == 'normal_test.csv':
+                            split_list = ['test'] * num_filenames
+
+                        if file == 'normal_train.csv' or file == 'normal_val.csv':
+                            mask_pos_list = [None] * num_filenames
+                            mask_neg_list = [None] * num_filenames
+                        else:
+                            mask_pos_file = os.path.join(split_path, 'normal_test_ann.csv')
+                            mask_pos_df = pd.read_csv(mask_pos_file)
+                            mask_pos_df['filename'] = mask_pos_df['filename'].str.replace('./data', path, regex=False)
+                            mask_pos_list = mask_pos_df['filename'].tolist()
+                            mask_neg_list = [None] * len(mask_pos_list)
+
+                        data_to_append = pd.DataFrame({
+                            'filename': filenames,
+                            'type': type_list,
+                            'full_map': full_map_list,
+                            'mask_pos': mask_pos_list,
+                            'mask_neg': mask_neg_list,
+                            'split': split_list
+                        })
+
+                        # Use concat instead of append
+                        data_df = pd.concat([data_df, data_to_append], ignore_index=True)
+
+                else:
+                    abnormal_class = file.split('.csv')[0]
+                    abnormal_class = abnormal_class.split('_ann')[0]
+                    abnormal_class = abnormal_class.split('_neg')[0]
+
+                    if class_names[abnormal_class] == 0:
+                        class_names[abnormal_class] = 1
+                        print(abnormal_class)
+                        img_file = os.path.join(split_path, (abnormal_class + '.csv'))
+                        img_neg_file = os.path.join(split_path, (abnormal_class + '_neg.csv'))
+                        img_pos_file = os.path.join(split_path, (abnormal_class + '_ann.csv'))
+
+                        filenames = []
+                        mask_neg_list = []
+                        mask_pos_list = []
+                        type_list = []
+                        split_list = []
+                        length = 0
+                        # check if the file exists
+                        if os.path.isfile(img_file):
+                            img_df = pd.read_csv(img_file)
+                            img_df['filename'] = img_df['filename'].str.replace('./data', path, regex=False)
+                            img_df['full_map'] = img_df['filename']
+                            img_df['full_map'] = img_df['full_map'].str.replace('.png', '_brain_map_full.png', regex=False)
+
+
+                            filenames = img_df['filename'].tolist()
+                            full_map_list = img_df['full_map'].tolist()
+                            
+                            num_filenames = len(filenames)
+                            type_list = [abnormal_class] * num_filenames
+                            split_list = ['test'] * num_filenames
+                            length = num_filenames
+
+                        if os.path.isfile(img_neg_file):
+                            img_neg_df = pd.read_csv(img_neg_file)
+                            img_neg_df['filename'] = img_neg_df['filename'].str.replace('./data', path, regex=False)
+                            mask_neg_list = img_neg_df['filename'].tolist()
+
+
+                        if os.path.isfile(img_pos_file):
+                            img_pos_df = pd.read_csv(img_pos_file)
+                            img_pos_df['filename'] = img_pos_df['filename'].str.replace('./data', path, regex=False)
+                            mask_pos_list = img_pos_df['filename'].tolist()
+
+                        if mask_neg_list == []:
+                            mask_neg_list = [None] * length
+                        if mask_pos_list == []:
+                            mask_pos_list = [None] * length
+                        if type_list == []:
+                            type_list = [None] * length
+                        if split_list == []:
+                            split_list = [None] * length
+
+
+                        data_to_append = pd.DataFrame({
+                            'filename': filenames,
+                            'type': type_list,
+                            'full_map': full_map_list,
+                            'mask_pos': mask_pos_list,
+                            'mask_neg': mask_neg_list,
+                            'split': split_list
+                        })
+                        # Use concat instead of append
+                        data_df = pd.concat([data_df, data_to_append], ignore_index=True)
+
+                    else:
+                        print('class already exists')
+                        print(abnormal_class)
+                        continue
+
+
+
+        return data_df
+
